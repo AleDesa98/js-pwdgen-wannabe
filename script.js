0a375b7d418983b008de93ff8a3a0596dd1bb0e0
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-04-26 14:23:57 +0200

    finito

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Come ti chiami?");
+var cognome = prompt("Qual'è il tuo cognome?");
+var colore = prompt("Colore preferito?");
+var anno = "21";
+
+document.getElementById('password').innerHTML = nome + cognome + colore + anno;
